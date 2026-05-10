12f45122aecd2b02cf8ca30cc8a3aba2c49350d0
sibilr1016 <sibilr1016@gmail.com>
2026-05-10 18:09:24 +0530

    profile partially completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="flex flex-col gap-1 md:flex-row justify-center items-center min-h-screen ">
+    <div className="flex flex-col gap-1 bg-gray-950 md:flex-row justify-center items-center min-h-screen ">
       <nav className="flex md:flex-col gap-1">
         <div className="bg-slate-800 p-2 flex md:flex-col md:py-4 items-center justify-center  rounded-lg">
           <ion-icon
@@ -27,11 +27,43 @@ function App() {
         </div>
       </nav>
       <div className="flex flex-col gap-1 lg:flex-row">
-        <section className="bg-slate-800 text-gray-50">
-          <img src="" alt="" />
-          <h1>SIBI LR</h1>
-          <p>Full-Stack Web Developer</p>
-          <div>icons</div>
+        <section className="bg-slate-800 text-gray-50 rounded-xl">
+          <div className="max-w-xs p-1 ">
+            <img className="rounded-xl" src="/cover.jpg" alt="" />
+          </div>
+          <div className="max-w-28 -mt-14 z-10 mx-auto border border-gray-950 rounded-lg">
+            <img className="rounded-lg" src="/profile-1.jpg" alt="" />
+          </div>
+          <div className="flex flex-col items-center justify-center mt-2">
+            <h1 className="tracking-wide font-medium text-gray-300 text-xl">
+              SIBI LR
+            </h1>
+            <p className="text-sm mb-4 text-gray-300">
+              Full-Stack Web Developer
+            </p>
+            <div className="flex gap-1 mb-4">
+              <div className="bg-gray-900 p-1 pb-0 text-lg rounded-md">
+                <ion-icon name="logo-linkedin"></ion-icon>
+              </div>
+              <div className="bg-gray-900 p-1 pb-0 text-lg rounded-md">
+                <ion-icon name="logo-github"></ion-icon>
+              </div>
+            </div>
+          </div>
+          <div className="mt-10 flex justify-between items-center px-5 mb-2 border-2 border-x-0 border-b-0 border-t-gray-700">
+            <div className="flex pt-2 gap-1">
+              <p className="text-sm">My Resume</p>
+              <div>
+                <ion-icon name="document-outline"></ion-icon>
+              </div>
+            </div>
+            <div className="flex gap-1">
+              <p className="text-sm">Contact Me</p>
+              <div>
+                <ion-icon name="call-outline"></ion-icon>
+              </div>
+            </div>
+          </div>
         </section>
         <section className="bg-violet-900">content</section>
       </div>
